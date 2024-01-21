54342c435e9c7cf2ed13393c4f7075693822b01f
Peter <mileshko.petr@yandex.ru>
2024-01-21 11:44:01 +0300

    updated slider
    Обработчик слайдера

--- source/js/app.js
+++ source/js/app.js
@@ -1,5 +1,11 @@
 //Вспомогательные функции
 // Удаление в контейнере классов по маске
+function removeClass(element, selector) {
+  if (element.classList.contains(selector)) {
+    element.classList.remove(selector);
+  }
+}
+
 function parentContainer(container, modifier) {
   const element = document.querySelector(container);
 
@@ -60,4 +66,46 @@ const initMobileMenu = menuMobile({
 initMobileMenu();
 //Бургер окончание
 
-parentContainer('.page__body', '--nojs'); // Удалить модификаторы, для отключенного JS в браузере, во всем документе
+// Слайдер
+function runSlides(container) {
+
+  const moveBack = container.querySelector('.slider__button--prev');
+  const moveForward = container.querySelector('.slider__button--next');
+  const slides = container.querySelectorAll('.slider__item');
+  let totalSlides;
+  if (moveBack !== null && moveForward !== null && slides.length) {
+    totalSlides = slides.length;
+    moveBack.addEventListener('click', e => {
+      e.preventDefault();
+      showPrevious();
+    });
+
+    moveForward.addEventListener('click', e => {
+      e.preventDefault();
+      showNext();
+    });
+  }
+
+  function showNext() {
+    let newIndex;
+    slides[slideIndex].dataset.slider = '';
+    newIndex = (slideIndex < (totalSlides - 1)) ? slideIndex + 1 : 0;
+    slides[newIndex].dataset.slider = 'active';
+    slideIndex = newIndex;
+  }
+
+  function showPrevious() {
+    let newIndex;
+    slides[slideIndex].dataset.slider = '';
+    newIndex = (slideIndex > 0) ? slideIndex - 1 : (totalSlides - 1);
+    slides[newIndex].dataset.slider = 'active';
+    slideIndex = newIndex;
+  }
+}
+
+const initSlider = parentContainer('.slider', '--nojs'); // первый параметр класс родительского контейнера, второй модификатор который надо удалить у дочерних
+
+const slider = initSlider();
+let slideIndex = 0;
+
+runSlides(slider);
